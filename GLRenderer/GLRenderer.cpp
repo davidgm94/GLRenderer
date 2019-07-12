e7c0a4d8df743720c81ac4ba5163a47f32015dab
David <davidgonzalezmartin94@gmail.com>
2019-07-12 23:20:00 +0200

    Hello rectangle

--- GLRenderer/GLRenderer.cpp
+++ GLRenderer/GLRenderer.cpp
@@ -125,18 +125,29 @@ int main()
 
 	float vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+	};
+
+	unsigned indices[] =
+	{
+		0, 1, 3,
+		1, 2, 3
 	};
 
 	unsigned vertexBuffer, vertexArray;
+	unsigned elementArrayBuffer;
 	glGenVertexArrays(1, &vertexArray);
 	glGenBuffers(1, &vertexBuffer);
+	glGenBuffers(1, &elementArrayBuffer);
 
 	glBindVertexArray(vertexArray);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementArrayBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), nullptr);
 	glEnableVertexAttribArray(0);
@@ -144,13 +155,16 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 	
+	// Wireframe mode
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vertexArray);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
